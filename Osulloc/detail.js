2b484ccd4f7d1fd9614d95e705d09fc173370c37
Heeseon1215 <skgmltjs01@naver.com>
2024-12-02 17:23:56 +0900

    1202 수정

---
+++ Osulloc/detail.js
@@ -0,0 +1,9 @@
+const rabbit = lottie.loadAnimation({
+  container: document.getElementById('rabbit2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: './images/rabbit2.json'
+});
+
+
--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -1,27 +1,17 @@
 // a href="#" 속성 없애기
-$('a[href="#"').on('click', function(e){
+$('a[href="#"]').on('click', function(e){
   e.preventDefault()
 });
-
 
 
 $('.gnb-btn').on('click', function () {
   // header 메뉴 아이콘 클릭 시 toggle-btn이 적용/미적용
   $('header').toggleClass('toggle-btn');
 
-  if ($('header').hasClass('toggle-btn')) {
-      // 메뉴가 열릴 때 스크롤 비활성화 및 페이지를 맨 위로 이동
-      $('body').css({ overflow: 'hidden' }); // 스크롤 비활성화
-      window.scrollTo({ top: 0, behavior: 'smooth' }); // 부드럽게 맨 위로 이동
-  } else {
-      // 메뉴가 닫힐 때 스크롤 활성화
-      $('body').css({ overflow: '' }); // 스크롤 복원
-  }
 
   // 각 메뉴 클릭 시 버튼 초기화, 메뉴 닫힘
   $('.gnb li').on('click', function () {
       $('header').removeClass('toggle-btn');
-      $('body').css({ overflow: '' }); // 스크롤 복원
   });
 });
 
@@ -30,7 +20,6 @@ let prevScroll = window.scrollY;
 
 $(window).on('scroll', function () {
   if ($('header').hasClass('toggle-btn')) {
-      // toggle-btn 활성화 상태에서는 스크롤 동작을 막음
       return;
   }
 
